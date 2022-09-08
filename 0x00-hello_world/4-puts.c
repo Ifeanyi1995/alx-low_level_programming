055d6b04274ee74c197b83387864ab6ecbcf28cb
Ifeanyi1995 <ifeanyisimeon25@gmail.com>
2022-09-08 14:10:40 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,19 @@
+nclude <stdio.h>
+
+/**
+ *
+ *  * main - printing in c
+ *
+ *   * Return: Always 0 (highly successful)
+ *
+ *    */
+
+int main(void)
+
+{
+
+	puts("\"Programming is like building a multilingual puzzle");
+
+	return (0);
+
+}
