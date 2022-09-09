8dc4c6bd01f484f99326209fbddf983bb68548c3
Ifeanyi1995 <ifeanyisimeon25@gmail.com>
2022-09-09 04:58:17 -0700

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,9 +1,8 @@
 #include <stdio.h>
-
 /**                                 
-* main - main block
-* Return: 0 (Good)                
+*main - mainblock
+*Return: 0(Good)                
 */              
 int main(void)
-{
+{ 
 	printf("with proper grammar, but the outcome is a piece of art,\n");            return (0);                                                             }
