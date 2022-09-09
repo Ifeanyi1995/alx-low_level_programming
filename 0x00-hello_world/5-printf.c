647c2a96c86941242400e73196021dbb7712a0b1
Ifeanyi1995 <ifeanyisimeon25@gmail.com>
2022-09-09 04:35:44 -0700

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,9 +1,12 @@
 #include <stdio.h>
+
 /**                                 
-*main - main block
-*Return: 0 (Good)                
+* main - main block
+*
+* Description: using the main function
+* this program prints "programming is like building a multilingual puzzle
+* Return: 0 (Good)                
 */              
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art,\n");            
-	return (0);                                                             }
+	printf("with proper grammar, but the outcome is a piece of art,\n");            return (0);                                                             }
