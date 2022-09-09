e0fa596ed8193a4bcd7eb733556194efdbf4ac66
Ifeanyi1995 <ifeanyisimeon25@gmail.com>
2022-09-08 23:26:39 -0700

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,5 @@
+#include <stdio.h>                                                              /**                                 
+*main - printing in c        
+*Return: 0                
+*/                                                                              int main(void)                                                                  {
+printf("with proper grammar, but the outcome is a piece of art,\n");            return (0);                                                                     }  
