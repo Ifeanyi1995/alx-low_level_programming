4cfded15385fbb7fd393b0d7fbbcfafa24803d30
Ifeanyi1995 <ifeanyisimeon25@gmail.com>
2022-09-09 23:58:24 -0700

    Positive anything is bette

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -2,19 +2,21 @@
 #include <stdlib.h>
 #include <time.h>
 /**
- * main - Entry point
+ * main - determine if number is positive or negative
  *
- * Return: Always sucess
+ *Description: program will assign a random number to the variable n each time it is executed
+ *
+ * Return: Always 0 (sucess)
  */
+
 int main(void)
 {
-
 int n;
 
 scrand(time(0));
 n = rand() - RAND_MAX / 2;
 
-if (n> 0)
+if (n > 0)
 	printf("%d is positive\n", n);
 else if (n == 0)
 	printf("%d is zero\n", n);
