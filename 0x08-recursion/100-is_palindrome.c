c79d7c147d241c17d36319504c2b4551441b1f14
Ifeanyi1995 <ifeanyisimeon25@gmail.com>
2022-09-28 16:30:54 -0700

    pan

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -39,6 +39,6 @@ int comparator(char *s, int n1, int n2)
 int is_palindrome(char *s)
 {
 	if (*s == '\0')
-		if (*s == '\0')
-			return (comparator(s, 0, _strlen_recursion(s) - 1));
+		return (1);
+	return (comparator(s, 0, _strlen_recursion(s) - 1));
 }
